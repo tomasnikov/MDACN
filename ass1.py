5d7ca82f7e61cab931d068f087740ce32d302fdc
Tómas Þorbjarnarson <tomas@dohop.com>
2019-03-05 17:22:25 +0100

    Updates

--- ass1.py
+++ ass1.py
@@ -1,13 +1,26 @@
 import csv
 from pdb import set_trace
+from pprint import pprint
 
 data = []
+nodes = set()
+links = set()
 
 with open('manufacturing_emails_temporal_network.csv') as csv_file:
   csv_reader = csv.reader(csv_file, delimiter=',')
   line_count = 0
   for row in csv_reader:
     if line_count != 0:
-      data.append({'node1': row[0], 'node2': row[1], 'timestamp': row[2]})
+      n1 = int(row[0])
+      n2 = int(row[1])
+      t = int(row[2])
+      data.append({'node1': n1, 'node2': n2, 'timestamp': t})
+      nodes.add(n1)
+      nodes.add(n2)
+      #links.add('-'.join([row[0],row[1]]))
+      links.add((n1,n2))
     line_count += 1
 set_trace()
+
+
+# 13: timestamps that every node gets infected by i for the first 80%
