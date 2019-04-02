adddfd9d52ff706a4dde0cf89bf43fb9def46ab9
thorunnarna <thorunnarna93@gmail.com>
2019-04-02 14:53:05 +0200

    RefugeeAdjacency start

--- Final Project/Scripts/RefugeeNetwork.py
+++ Final Project/Scripts/RefugeeNetwork.py
@@ -1,5 +1,6 @@
 import xml.etree.ElementTree as ET
 from pdb import set_trace
+import numpy as np
 
 tree = ET.parse('..\data\RefugeeData.xml')
 RefugeeXML = tree.getroot()
@@ -12,11 +13,51 @@ for root in RefugeeXML:
         row =[]
         count = 0
         for field in record:
-            if count <4:    
+            if count <4:  
                 row.append(field.text)
                 count+=1
         dataMatrix.append(row) 
 
+dataObject = []
+allCountries= []
+years =set()
 
-print(count)
+
+for data in dataMatrix:
+        entry = {
+        'Residence':data[0],
+        'Origin':data[1],
+        'Year':data[2],
+        'Count':data[3]
+        }
+        dataObject.append(entry)
+        years.add(entry['Year'])
+        if entry['Residence'] not in allCountries:
+                allCountries.append(entry['Residence'])
+        if entry['Origin'] not in allCountries:
+                allCountries.append(entry['Origin'])
+      
+
+numCountries = len(allCountries)
+allCountries.sort()
+nodes = allCountries
+linkIndices = []
+linkNames = []
+
+
+countyear = -1
+years =["2016"]
+adjacency = np.zeros((numCountries,numCountries))
+for year in years:
+        countyear+=1
+        onlyNew =list(filter(lambda x:x['Year']==year,dataObject))
+        for i,country in enumerate(nodes):
+                for j,otherCountry in enumerate(nodes):
+                        if i !=j:
+                                i_relevant =list(filter(lambda x:x['Residence']==nodes[i],onlyNew))
+                                j_relevant =list(filter(lambda x:x['Origin']==nodes[j],i_relevant))
+                                if j_relevant != []:
+                                
+                                        adjacency[i,j] = j_relevant[0]['Count']
 set_trace()
+np.savetxt("RefugeeAdjacency.csv", adjacency, delimiter=",")
