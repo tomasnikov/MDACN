150d21276a49ef6edb52aaa735dbc0dff9ab1a65
thorunnarna <thorunnarna93@gmail.com>
2019-04-02 15:23:45 +0200

    RefugeeCountries

--- Final Project/Scripts/RefugeeNetwork.py
+++ Final Project/Scripts/RefugeeNetwork.py
@@ -1,6 +1,7 @@
 import xml.etree.ElementTree as ET
 from pdb import set_trace
 import numpy as np
+import pandas
 
 tree = ET.parse('..\data\RefugeeData.xml')
 RefugeeXML = tree.getroot()
@@ -39,17 +40,21 @@ for data in dataMatrix:
       
 
 numCountries = len(allCountries)
+
 allCountries.sort()
+df = pandas.DataFrame(data={"Countries": allCountries})
+df.to_csv("./RefugeeCountries.csv", sep=',',index=False)
 nodes = allCountries
 linkIndices = []
 linkNames = []
-
+set_trace()
 
 countyear = -1
 years =["2016"]
 adjacency = np.zeros((numCountries,numCountries))
 for year in years:
         countyear+=1
+        '''
         onlyNew =list(filter(lambda x:x['Year']==year,dataObject))
         for i,country in enumerate(nodes):
                 for j,otherCountry in enumerate(nodes):
@@ -59,5 +64,15 @@ for year in years:
                                 if j_relevant != []:
                                 
                                         adjacency[i,j] = j_relevant[0]['Count']
-set_trace()
+
+        '''
+        for i,country in enumerate(nodes):
+                for j,otherCountry in enumerate(nodes):
+                        if i !=j:
+                                
+                                #relevant = list(filter(lambda x:(x['Residence']==nodes[i])&(x['Residence']==nodes[j])&(x['Year']==year),dataObject))  
+                                relevant = list(filter(lambda x:(x['Year']==year)&(x['Residence']==nodes[i])&(x['Origin']==nodes[j]),dataObject))
+                                if relevant != []:
+                                        adjacency[i,j] = relevant[0]['Count']
+                                        
 np.savetxt("RefugeeAdjacency.csv", adjacency, delimiter=",")
